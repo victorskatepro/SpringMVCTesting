d477150ee51631569d9f5433ec9de6e0f85a8e3e
alejandro <victor.saico@tecsup.edu.pe>
2017-10-18 23:06:41 -0500

    Laboratorio 10

---
+++ src/main/java/com/tecsup/gestion/exception/DAOException.java
@@ -0,0 +1,14 @@
+package com.tecsup.gestion.exception;
+
+
+public class DAOException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public DAOException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+
+}
+
