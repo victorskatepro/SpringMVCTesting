fa6f2203b1e7ea5280eba09c5c1993af1facee98
alejandro <victor.saico@tecsup.edu.pe>
2017-10-31 11:57:30 -0500

    Lab10

--- src/main/java/com/tecsup/gestion/dao/EmployeeDAO.java
+++ src/main/java/com/tecsup/gestion/dao/EmployeeDAO.java
@@ -1,5 +1,9 @@
 package com.tecsup.gestion.dao;
 
+
+
+import java.util.List;
+
 import com.tecsup.gestion.exception.DAOException;
 import com.tecsup.gestion.exception.EmptyResultException;
 import com.tecsup.gestion.model.Employee;
@@ -7,5 +11,19 @@ import com.tecsup.gestion.model.Employee;
 
 public interface EmployeeDAO {
            Employee findEmployee(int id) throws DAOException, EmptyResultException;
-          
+           
+         void create(String login, String password, String lastname, String firstname, int salary, int dptId) throws DAOException;
+
+       	void delete(String login) throws DAOException;
+
+       	void update(String login, String password, String lastname, String firstname, int salary, int dptId) throws DAOException;
+
+       	Employee findEmployeeByLogin(String login) throws DAOException, EmptyResultException;
+       	
+       	Employee findEmployeeByLastaname(String lastname) throws DAOException, EmptyResultException;
+
+       	List<Employee> findAllEmployees() throws DAOException, EmptyResultException;
+
+       	List<Employee> findEmployeesByName(String name) throws DAOException, EmptyResultException;
+
 }
--- src/main/java/com/tecsup/gestion/dao/jdbc/EmployeeDAOImpl.java
+++ src/main/java/com/tecsup/gestion/dao/jdbc/EmployeeDAOImpl.java
@@ -1,4 +1,6 @@
 package com.tecsup.gestion.dao.jdbc;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,8 +26,7 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 	@Override
 	public Employee findEmployee(int employee_id) throws DAOException, EmptyResultException {
 
-		String query = "SELECT employee_id, login, password, first_name, last_name, salary, department_id "
-				+ " FROM employees WHERE employee_id = ?";
+		String query = "SELECT employee_id, login, password, first_name, last_name, salary, department_id FROM employees WHERE employee_id = ?";
 
 		Object[] params = new Object[] { employee_id };
 
@@ -42,5 +43,138 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 			throw new DAOException(e.getMessage());
 		}
 	}
+
+
+	@Override
+	public void create(String login, String password, String lastname, String firstname, int salary, int dptId) throws DAOException {
+
+		String query = "INSERT INTO employees (login, password, first_name, last_name, salary, department_id)  VALUES ( ?,?,?,?,?,? )";
+
+		Object[] params = new Object[] { login, password, lastname, firstname, salary, dptId };
+
+		Employee emp = null;
+		
+		try {
+			// create
+			jdbcTemplate.update(query, params);
+			// search
+			emp = this.findEmployeeByLogin(login);
+
+		} catch (EmptyResultException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			logger.info("Error: " + e.getMessage());
+			throw new DAOException(e.getMessage());
+		}
+		
+
+	}
+
+	@Override
+	public void delete(String login) throws DAOException {
+
+		String query = "DELETE FROM  employees WHERE login = ? ";
+
+		Object[] params = new Object[] { login };
+
+		try {
+			jdbcTemplate.update(query, params);
+		} catch (Exception e) {
+			logger.info("Error: " + e.getMessage());
+			throw new DAOException(e.getMessage());
+		}
+	}
+
+	@Override
+	public void update(String  login, String password, String lastname, String firstname, int salary, int dptId) throws DAOException {
+
+		String query = "UPDATE employees SET password = ?, first_name =?, last_name = ?, salary = ?, department_id = ? WHERE login = ?";
+
+		Object[] params = new Object[] { password, lastname, firstname, salary, dptId, login };
+
+		try {
+			jdbcTemplate.update(query, params);
+		} catch (Exception e) {
+			logger.info("Error: " + e.getMessage());
+			throw new DAOException(e.getMessage());
+		}
+	}
+
+
+	@Override
+	public Employee findEmployeeByLogin(String login) throws DAOException, EmptyResultException {
+
+		String query = "SELECT employee_id, login, password, first_name, last_name, salary, department_id FROM employees WHERE login = ? ";
+
+		Object[] params = new Object[] { login };
+		try {
+			Employee employee = jdbcTemplate.queryForObject(query, params, new EmployeeMapper());
+			//
+			return employee;
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new EmptyResultException();
+		} catch (Exception e) {
+			logger.info("Error: " + e.getMessage());
+			throw new DAOException(e.getMessage());
+		}
+	}
+	
+	@Override 
+	public Employee findEmployeeByLastaname(String lastname) throws DAOException, EmptyResultException{
+	   String query = "SELECT employee_id, login, password, first_name, last_name, salary, department_id FROM employees WHERE last_name = ? ";
+	Object[] params = new Object[] { lastname};
+	try {
+		
+		Employee employee = jdbcTemplate.queryForObject(query, params, new EmployeeMapper());
+		return employee;
+	}catch (EmptyResultDataAccessException e) {
+		throw new EmptyResultException();
+	}catch (Exception e) {
+		logger.info("Error: "+ e.getMessage());
+		throw new DAOException(e.getMessage());
+	}
+	}
+	@Override
+	public List<Employee> findAllEmployees() throws DAOException, EmptyResultException {
+
+		String query = "SELECT employee_id, login, password, first_name, last_name, salary, department_id FROM employees ";
+
+		try {
+
+			List<Employee> employees = jdbcTemplate.query(query, new EmployeeMapper());
+			//
+			return employees;
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new EmptyResultException();
+		} catch (Exception e) {
+			logger.info("Error: " + e.getMessage());
+			throw new DAOException(e.getMessage());
+		}
+	}
+
+	@Override
+	public List<Employee> findEmployeesByName(String name) throws DAOException, EmptyResultException {
+
+		String query = "SELECT employee_id, login, password, first_name, last_name, salary, department_id FROM employees WHERE upper(first_name) like upper(?) ";
+
+		Object[] params = new Object[] { "%" + name + "%" };
+
+		try {
+
+			List<Employee> employees = jdbcTemplate.query(query, params, new EmployeeMapper());
+			//
+			return employees;
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new EmptyResultException();
+		} catch (Exception e) {
+			logger.info("Error: " + e.getMessage());
+			throw new DAOException(e.getMessage());
+		}
+	}
+
 
 }
